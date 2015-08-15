f8fd9196f41e142d4ea593d89191d46a6cbf3b84
Honghao Zhang <zhh358@gmail.com>
2015-08-15 13:34:45 -0400

    Finished local parsing

--- UW Info Session 2.0/UW Info Session/UW Info Session/Network/InfoSessionSourceHTMLParser.swift
+++ UW Info Session 2.0/UW Info Session/UW Info Session/Network/InfoSessionSourceHTMLParser.swift
@@ -16,6 +16,10 @@ struct InfoSessionSourceHTMLParser {
     let kTime = "Time"
     let kLocation = "Location"
     let kWebSite = "Web Site"
+    let kAudience = "Audience"
+    let kProgram = "Program"
+    let kDescription = "Description"
+    let kId = "id"
     
     var result = [String: AnyObject]()
     
@@ -30,59 +34,77 @@ struct InfoSessionSourceHTMLParser {
         let nodes = doc.xPath("//*[@id='tableform']")
         if let tableNode = nodes?.first where tableNode.name == "table" {
             // Divide trs into different sessions
-            // Each session is a list of string, which contains raw content of tr
-            var sessions = [[String]]()
-            var session: [String]?
+            // Each session is a list of tr Ji node
+            var trSessionGroups = [[JiNode]]()
+            var trSessionGroup: [JiNode]?
             for tr in tableNode {
-                if var content = tr.content {
-                    if content.hasPrefix("\(kEmployer):") {
-                        if let session = session { sessions.append(session) }
-                        session = [content]
+                if var tdContent = tr.firstChildWithName("td")?.content {
+                    if tdContent.hasPrefix("\(kEmployer):") {
+                        if let trSessionGroup = trSessionGroup { trSessionGroups.append(trSessionGroup) }
+                        trSessionGroup = [tr]
                         continue
                     }
-                    session!.append(content)
+                    trSessionGroup!.append(tr)
                 }
             }
             
-            // Process each session to a dictionary
-//            log.debug(sessions)
-            let json = JSON(sessions.map { self.processSession($0) })
+            // Process each session group to a dictionary
+            let json = JSON(trSessionGroups.map { self.processTrSessionGroupToDict($0) })
             log.debug(json)
         }
     }
     
-    private func processSession(session: [String]) -> [[String: String]] {
+    private func processTrSessionGroupToDict(trSession: [JiNode]) -> [[String: String]] {
         var result = [[String: String]]()
         
-        for eachString in session {
-            if eachString.hasPrefix("\(kEmployer):") {
-                result.append(splitSessionString(eachString, forKey: kEmployer))
-            } else if eachString.hasPrefix("\(kDate):") {
-                result.append(splitSessionString(eachString, forKey: kDate))
-            } else if eachString.hasPrefix("\(kTime):") {
-                result.append(splitSessionString(eachString, forKey: kTime))
-            } else if eachString.hasPrefix("\(kLocation):") {
-                result.append(splitSessionString(eachString, forKey: kLocation))
-            } else if eachString.hasPrefix("\(kWebSite):") {
-                result.append(splitSessionString(eachString, forKey: kWebSite))
-            } else {
-                // TODO:
+        var webSiteIndex: Int?
+        for (index, tr) in enumerate(trSession) {
+            if let firstString = tr.firstChild?.content?.trimmed() where firstString.hasPrefix("\(kEmployer):") {
+                let secondString = tr.firstChild?.nextSibling?.content?.trimmed()
+                result.append([kEmployer: secondString ?? "null"])
+            } else if let firstString = tr.firstChild?.content?.trimmed() where firstString.hasPrefix("\(kDate):") {
+                let secondString = tr.firstChild?.nextSibling?.content?.trimmed()
+                result.append([kDate: secondString ?? "null"])
+            } else if let firstString = tr.firstChild?.content?.trimmed() where firstString.hasPrefix("\(kTime):") {
+                let secondString = tr.firstChild?.nextSibling?.content?.trimmed()
+                result.append([kTime: secondString ?? "null"])
+            } else if let firstString = tr.firstChild?.content?.trimmed() where firstString.hasPrefix("\(kLocation):") {
+                let secondString = tr.firstChild?.nextSibling?.content?.trimmed()
+                result.append([kLocation: secondString ?? "null"])
+            } else if let firstString = tr.firstChild?.content?.trimmed() where firstString.hasPrefix("\(kWebSite):") {
+                var secondString = tr.firstChild?.nextSibling?.content?.trimmed()
+                if secondString == "http://" { secondString = "" }
+                result.append([kWebSite: secondString ?? "null"])
+                webSiteIndex = index
+            }
+            else if let webSiteIndex = webSiteIndex where index == webSiteIndex + 1 {
+                // Audience + Programs
+                if let rawContent = tr.xPath("./td/i").first?.rawContent?.replaceMatches("<(i|/i)>", withString: "", ignoreCase: false) {
+                    let components = rawContent.componentsSeparatedByString("<br>")
+                    if components.count == 3 {
+                        let levelString = (components[0].hasPrefix("For ") ? components[0].stringByReplacingOccurrencesOfString("For ", withString: "", options: NSStringCompareOptions(0), range: nil) : components[0]).trimmed()
+                        let studentString = components[1].replaceMatches(", ", withString: ",", ignoreCase: true)?.stringByReplacingOccurrencesOfString(",", withString: ", ", options: NSStringCompareOptions(0), range: nil).trimmed() ?? ""
+                        let programString = components[2].trimmed()
+                        result.append([kAudience: "\(levelString) \(studentString)".trimmed()])
+                        result.append([kProgram: programString])
+                    } else {
+                        log.error("Parsing Audient and Program failed.")
+                    }
+                } else {
+                    log.error("Get raw text for Audience failed.")
+                }
+            } else if let webSiteIndex = webSiteIndex where index == webSiteIndex + 2 {
+                // Description
+                if let rawContent = tr.xPath("./td/i").first?.rawContent?.replaceMatches("<(i|/i)>", withString: "", ignoreCase: false) {
+                    let removedBrString = rawContent.stringByReplacingOccurrencesOfString("<br>", withString: "\n", options: NSStringCompareOptions(0), range: nil)
+                    result.append([kDescription: removedBrString])
+                } else {
+                    log.error("Get raw text for Audience failed.")
+                }
             }
         }
         
         return result
-    }
-    
-    private func splitSessionString(string: String, forKey key: String) -> [String: String] {
-        let components = string.componentsSeparatedByString(": \r\n")
-        if components.count == 2 {
-            return [key: components[1].trimmed()]
-        } else if components.count == 1 {
-            return [key: ""]
-        } else {
-            log.error("\(key) split failed")
-            return [key: "null"]
-        }
     }
 }
 
@@ -92,8 +114,8 @@ struct InfoSessionSourceHTMLParser {
 // 2: Time √
 // 3: Location √
 // 4: Web Site √
-// 5: Audiences + Programm
-// 6: Description
+// 5: Audiences + Programm √
+// 6: Description √
 // 7: Attendance ???
 // 8: Bakc to listing
 // 9: You are not logged in
