8f7384dcf1c1d53cecf3ac995686ca713b22b0ae
Honghao Zhang <zhh358@gmail.com>
2016-12-22 22:51:27 -0500

    Setup new v2 project

---
+++ UW-Info-Session/Pods/Target Support Files/Pods-UW-Info-SessionTests/Pods-UW-Info-SessionTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UW_Info_SessionTests : NSObject
+@end
+@implementation PodsDummy_Pods_UW_Info_SessionTests
+@end
---
+++ UW-Info-Session/Pods/Target Support Files/Pods-UW-Info-SessionUITests/Pods-UW-Info-SessionUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UW_Info_SessionUITests : NSObject
+@end
+@implementation PodsDummy_Pods_UW_Info_SessionUITests
+@end
